6b160377f45c495e8cad850baa74d0eb0beaf661
Tazz <charlesaberdo@gmail.com>
2025-11-19 05:34:29 +0800

    init

---
+++ src/main/java/oopg2/academic/dashboard/AcademicDashboard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oopg2.academic.dashboard;
+
+/**
+ *
+ * @author Charles Ian
+ */
+public class AcademicDashboard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
