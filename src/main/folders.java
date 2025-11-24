532d885dcdf5122756e746704ea68686738b5fd9
Tazz <charlesaberdo@gmail.com>
2025-11-25 04:23:36 +0800

    Update, Build - menu, notes, assets

---
+++ src/main/folders.java
@@ -0,0 +1,32 @@
+package main;
+
+public class folders extends javax.swing.JPanel {
+
+    public folders() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton2 = new javax.swing.JButton();
+
+        setLayout(new javax.swing.OverlayLayout(this));
+
+        jButton2.setText("jButton2");
+        jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton2.setPreferredSize(new java.awt.Dimension(125, 125));
+        jButton2.addActionListener(this::jButton2ActionPerformed);
+        add(jButton2);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    // End of variables declaration//GEN-END:variables
+}
